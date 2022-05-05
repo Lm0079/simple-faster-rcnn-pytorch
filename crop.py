a2ea32c21233b73db06f5a8de12d0a622ef2a897
lm0079 <gh18931@bristol.ac.uk>
2022-05-05 11:56:50 +0100

    Evaluation scripts added

--- crop.py
+++ crop.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import os
 import torch as t
 from utils.config import opt
@@ -5,51 +6,59 @@ from model import FasterRCNNVGG16
 from trainer import FasterRCNNTrainer
 from data.util import  read_image
 from utils.vis_tool import vis_bbox
+import argparse
+from pathlib import Path
 from utils import array_tool as at
 import cv2
+parser = argparse.ArgumentParser(
+	description=" Ablation Cropper",
+	formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+)
+parser.add_argument(
+	"--data",
+	type=Path,
+	help=""
+)
 faster_rcnn = FasterRCNNVGG16()
 trainer = FasterRCNNTrainer(faster_rcnn).cuda()
-data = '/user/work/gh18931/diss/datasets/zebra_test_set/'
-model_names = ['/user/work/gh18931/diss/simple-faster-rcnn-pytorch/checkpoints_epoch_14/fasterrcnn_04171116_0.8256965596830876']
-for model_name in model_names :
-	print("model loading")
-	trainer.load(model_name)
+def main(args):
+	model_names = ['/user/work/gh18931/diss/simple-faster-rcnn-pytorch/checkpoints/fasterrcnn_05011331_0.8941059978840188']
+	for model_name in model_names :
+		print("model loading")
+		trainer.load(model_name)
 
-	model_n = model_name.split("/")[-1]
-	output_p = f"cropped/{model_n}"
-	isExist = os.path.exists(output_p)
-	if not isExist:
-		os.makedirs(output_p)
-	valid_img = ".jpg"
-	for f in os.listdir(data):
-		ext = os.path.splitext(f)[1]
-		if ext.lower() != valid_img:
-			continue
-		print(f)
-		file = data + f
-		img = read_image(file)
-		img = t.from_numpy(img)[None]
+		valid_img = ".jpg"
+		for f in os.listdir(args.data):
+			ext = os.path.splitext(f)[1]
+			if ext.lower() != valid_img:
+				continue
+			if "cropped" in f:
+				continue
+			print(f)
+			file = str(args.data) +"/"+ f
+			img = read_image(file)
+			img = t.from_numpy(img)[None]
 
-		opt.caffe_pretrain=False # this model was trained from torchvision-pretrained model
-		_bboxes, _labels, _scores = trainer.faster_rcnn.predict(img,visualize=True)
-		
-		for labels in _labels:
+			opt.caffe_pretrain=False # this model was trained from torchvision-pretrained model
+			_bboxes, _labels, _= trainer.faster_rcnn.predict(img,visualize=True)
 			
-			found = False
-			for count ,label in enumerate(labels):
-				image = cv2.imread(file)
+			for labels in _labels:
+				
+				found = False
+				for count ,label in enumerate(labels):
+					image = cv2.imread(file)
 
-				if label == 1 and not found:
-					bbox = _bboxes[0][count]
-					
-					found=True
-					cropped_img = image[int(bbox[0]) : int(bbox[2]-bbox[0]), int(bbox[1]):int(bbox[3] - bbox[1])]
-					output_f = os.path.join(output_p , f"{f}")
-					if not cropped_img.any() : 
-						pass
-					else:
-						cv2.imwrite(output_f,cropped_img)
+					if label == 1 and not found:
+						bbox = _bboxes[0][count]
+						
+						found=True
+						cropped_img = image[int(bbox[0]) : int(bbox[2]), int(bbox[1]):int(bbox[3] )]
+						output_f = os.path.join(str(args.data) , f"cropped_{f}")
+						if not cropped_img.any() : 
+							pass
+						else:
+							cv2.imwrite(output_f,cropped_img)
 
 					
-					
-	
+if __name__ == "__main__":
+	main(parser.parse_args())
--- eval.py
+++ eval.py
@@ -6,17 +6,17 @@ from trainer import FasterRCNNTrainer
 from data.util import  read_image
 from utils.vis_tool import vis_bbox
 from utils import array_tool as at
-
+import cv2
 faster_rcnn = FasterRCNNVGG16()
 trainer = FasterRCNNTrainer(faster_rcnn).cuda()
-data = '/user/work/gh18931/diss/datasets/zebra_test_set/'
-model_names = ['/user/work/gh18931/diss/simple-faster-rcnn-pytorch/checkpoints_epoch_14/fasterrcnn_04171114_0.7569560460681852','/user/work/gh18931/diss/simple-faster-rcnn-pytorch/checkpoints_epoch_14/fasterrcnn_04171115_0.7898412502829089','/user/work/gh18931/diss/simple-faster-rcnn-pytorch/checkpoints_epoch_14/fasterrcnn_04171116_0.8256965596830876','/user/work/gh18931/diss/simple-faster-rcnn-pytorch/checkpoints_epoch_14/fasterrcnn_04171116_0.8263813451381766','/user/work/gh18931/diss/simple-faster-rcnn-pytorch/checkpoints_epoch_14/fasterrcnn_04171119_0.8714583358748896','/user/work/gh18931/diss/simple-faster-rcnn-pytorch/checkpoints_epoch_14/fasterrcnn_04171121_0.8831425095611765']
+data = '/user/work/gh18931/diss/test/imgs/'
+model_names = ["/user/work/gh18931/diss/simple-faster-rcnn-pytorch/checkpoints/fasterrcnn_05011331_0.8941059978840188","/user/work/gh18931/diss/simple-faster-rcnn-pytorch/checkpoints/fasterrcnn_05011328_0.8547641971555016","/user/work/gh18931/diss/simple-faster-rcnn-pytorch/checkpoints/fasterrcnn_05011327_0.8524188234686645","/user/work/gh18931/diss/simple-faster-rcnn-pytorch/checkpoints/fasterrcnn_05011321_0.8895999193454125","/user/work/gh18931/diss/simple-faster-rcnn-pytorch/checkpoints/fasterrcnn_05011319_0.8258197446281452"]
 for model_name in model_names :
 	print("model loading")
 	trainer.load(model_name)
 
 	model_n = model_name.split("/")[-1]
-	output_p = f"output_2/{model_n}/"
+	output_p = f"output_3/{model_n}/"
 	isExist = os.path.exists(output_p)
 	if not isExist:
 		os.makedirs(output_p)
@@ -25,7 +25,7 @@ for model_name in model_names :
 		ext = os.path.splitext(f)[1]
 		if ext.lower() != valid_img:
 			continue
-		print(f)
+		#print(f)
 		file = data + f
 		img = read_image(file)
 		img = t.from_numpy(img)[None]
@@ -34,11 +34,18 @@ for model_name in model_names :
 		_bboxes, _labels, _scores = trainer.faster_rcnn.predict(img,visualize=True)
 
 		for labels in _labels:
+			found = False
 			for count ,label in enumerate(labels):
-				if label == 1:
-					
+				if label == 1 and not found:
+					image = cv2.imread(file)
+					print(file)
 					ax = vis_bbox(at.tonumpy(img[0]),
 						[list(_bboxes[0][count])],
 						[_labels[0][count]],
 						[_scores[0][count]])
 					ax.figure.savefig(f"{output_p}{f}")
+					bbox = _bboxes[0][count]
+					found=True
+					print(bbox)
+					cropped_img = image[int(bbox[0]) : int(bbox[2]), int(bbox[1]):int(bbox[3] )]
+					cv2.imwrite(f"{output_p}cropped_{f}",cropped_img)
